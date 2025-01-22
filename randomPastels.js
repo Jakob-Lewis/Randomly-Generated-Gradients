ed7dbde7de7b4ed6e72f37500c8217b90a095836
Jakob-Lewis <jakoblewis3000@gmail.com>
2025-01-22 12:23:15 -0600

    Update randomPastels.js
    added some comments

--- randomPastels.js
+++ randomPastels.js
@@ -11,10 +11,13 @@ function applyGradient() {
     for (let i = 0; i < 5; i++) {
         colors.push(getRandomPastelColor());
     }
+    // i < 5 sets the amount of colors to be used
+    // the following code is to apply the gradient around the text
     const gradient = `linear-gradient(to right, ${colors.join(', ')})`;
     nameElement.style.background = gradient;
     nameElement.style.webkitBackgroundClip = 'text';
     nameElement.style.webkitTextFillColor = 'transparent';
 }
+// this assigns a click event to the button changeColorButton thus allowing us to have different pastels
  document.getElementById('changeColorButton').addEventListener('click', applyGradient);
 applyGradient();
